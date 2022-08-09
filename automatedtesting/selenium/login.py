57b3cb23cd5bc92f2b03b3460c75f194caaca457
oyin <oyindaakinyemi@gmail.com>
2022-08-09 02:09:59 +0100

    fix: added logging to login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -5,10 +5,10 @@ from selenium.webdriver.common.by import By
 import logging
 
 print('Starting the browser...')
-options = ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
-
+# options = ChromeOptions()
+# options.add_argument("--headless")
+# driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
@@ -30,31 +30,31 @@ def login(user, password):
     products_title = driver.find_element(
         By.CSS_SELECTOR, "div[id='header_container'] > div > span.title").text
     if "PRODUCTS" in products_title:
-        print("Login successfully. Navigating to the demo page to products")
+        logging.info("Login successfully. Navigating to the demo page to products")
     else:
-        print('ERROR! when login you in!')
+        logging.info('ERROR! when login you in!')
 
 
 def add_products():
     title_products = driver.find_elements(
         By.CSS_SELECTOR, "div[class='inventory_item_name']")
     for title in title_products:
-        print('Adding product {}'.format(title.text))
+        logging.info('Adding product {}'.format(title.text))
     buttons_add_to_cart = driver.find_elements(
         By.CSS_SELECTOR, "button[class='btn btn_primary btn_small btn_inventory']")
     for button in buttons_add_to_cart:
         button.click()
 
-    print('Successfully added all products')
+    logging.info('Successfully added all products')
 
     for title in title_products:
-        print('Removing product {}'.format(title.text))
+        logging.info('Removing product {}'.format(title.text))
     buttons_remove_from_cart = driver.find_elements(
         By.CSS_SELECTOR, "button[class='btn btn_secondary btn_small btn_inventory']")
     for button in buttons_remove_from_cart:
         button.click()
 
-    print('Successfully removed all products')
+    logging.info('Successfully removed all products')
 
 
 login('standard_user', 'secret_sauce')
